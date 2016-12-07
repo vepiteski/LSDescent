88f3a9ddfdc81f3c915fcaf0d40289d1153c0a06
JPD <vepiteski@gmail.com>
2016-12-07 15:48:39 -0500

    initial commit

--- src/LSDescentMethods.jl
+++ src/LSDescentMethods.jl
@@ -1,5 +1,17 @@
 module LSDescentMethods
 
-# package code goes here
+using NLPModels
+using Optimize
+using LinearOperators
+
+
+AbstractLineFunction = Union{C1LineFunction,C2LineFunction}
+
+include("lbfgs.jl")
+include("armijo_wolfe.jl")
+
+using LineSearch
+
+
 
 end # module
