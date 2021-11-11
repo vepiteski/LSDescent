54fc3ca9c626359236c34863645e104ee333da4d
vepiteski <vepiteski@gmail.com>
2021-11-11 17:19:35 -0500

    Get logging 2

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,7 +27,7 @@ stp.meta.rtol = 0
 
 #include("Newton/NewtonStop.jl")
 
-stp = Newton_Stop(nlp, stp = stp);
+stp = bfgs_Stop(nlp, stp = stp);
 @show stp.meta.nb_of_stop, norm(stp.current_state.gx), stp.current_state.fx
 @show nlp.counters
 
