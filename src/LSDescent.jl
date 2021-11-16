02d696b219998c92b8672c0b8318da2ab8acc57c
vepiteski <vepiteski@gmail.com>
2021-11-16 14:55:22 -0500

    Revise exports

--- src/LSDescent.jl
+++ src/LSDescent.jl
@@ -1,6 +1,8 @@
 module LSDescent
 
-export bfgs, bfgs_Stop, bfgs_StopLS, L_bfgs_StopLS, Newton_StopLS, Newton_Stop, Newton_Spectral
+export bfgs, bfgs_Stop, bfgs_StopLS
+export L_bfgs, L_bfgs_Stop, L_bfgs_StopLS
+export Newton_StopLS, Newton_Stop, Newton_Spectral
 
 using LinearAlgebra
 
