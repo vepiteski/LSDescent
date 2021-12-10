ae3ab7eca2d15e062e503da010562d8307feb62d
vepiteski <vepiteski@gmail.com>
2021-12-10 11:53:08 -0500

    Typo 2

--- src/LSDescent.jl
+++ src/LSDescent.jl
@@ -2,6 +2,7 @@ module LSDescent
 
 export bfgs, bfgs_Stop, bfgs_StopLS
 export L_bfgs, L_bfgs_Stop, L_bfgs_StopLS
+export M_bfgs, M_bfgs_Stop, M_bfgs_StopLS
 export Newton_StopLS, Newton_Stop, Newton_Spectral
 export CG_generic, formula_FR, formula_PR, formula_HS, formula_HZ
 export CG_FR, CG_PR, CG_HS, CG_HZ
