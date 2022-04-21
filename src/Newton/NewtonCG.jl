33d74869d8d9924687d2048c7ae19cb185b54764
Jean-Pierre Dussault <dusj1701@dinf-dussau-03i.DInf.fsci.usherbrooke.ca>
2022-04-20 21:06:38 -0400

    Experiment with NewtonCG

--- src/Newton/NewtonCG.jl
+++ src/Newton/NewtonCG.jl
@@ -5,7 +5,8 @@ function NwtdirectionCG(H, ∇f; τ = 0.5, kwargs...)
     e=1e-8
     n = length(∇f)
     #τ = 0.5 # need parametrization
-    cgtol = max(e, min(0.7, 0.01 * norm(∇f)^(1.0 + τ)))
+    ng = norm(∇f)
+    cgtol = max(e, min(0.7, (0.1 * ng), ng^(1.0 + τ)))
     
     (d, cg_stats) = cg(H, -∇f,
                        atol=cgtol, rtol=0.0,
