ee0bb39c01d7b18f2e58a94bbd4363126a61f83d
Jean-Pierre Dussault <dusj1701@dinf-dussau-03i.DInf.fsci.usherbrooke.ca>
2022-04-29 11:27:48 -0400

    Ajouter genrose dans les tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,11 +8,22 @@ using SolverCore, Logging
 using LinearAlgebra
 
 # test all solvers with the well known Woods test function
+println(" Tests with the woods test function, n=40.\n)
 include("woods.jl")
 nlp = MathOptNLPModel(woods(40), name="woods")
 n = nlp.meta.nvar
 
 include("tests.jl")
+
+# test all solvers with the well known genrose test function
+println(" Tests with the genrose test function, n=40.\n)
+include("woods.jl")
+nlp = MathOptNLPModel(genrose(40), name="genrose")
+n = nlp.meta.nvar
+
+include("tests.jl")
+
+
 
 #println("Testing bfgs solvers\n\n")
 #include("Debugbfgs.jl")
